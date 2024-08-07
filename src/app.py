ef3974a8240956e5bb3ea10f0396e7fbe16264ca
Aayush Rana <aayushranahardi@gmail.com>
2024-08-07 22:21:29 +0530

    fixed

--- src/app.py
+++ src/app.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
 import uvicorn
 from routes import HousePriceRoute
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173",
+        "http://localhost:8000",
+],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
 
 
 @app.get("/")
 def index():
-    return 'Welcome to house data india.'
+    return {'message':'Welcome to house data india.'}
 
 
 app.include_router(HousePriceRoute.router)
